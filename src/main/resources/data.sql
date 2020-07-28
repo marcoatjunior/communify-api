d3132fc2a98a224f3b16c6ecb3f2dd1783b607e2
Marco Taborda <marco.taborda@dbserver.com.br>
2020-07-28 15:34:06 -0300

    Remove informações estáticas

--- src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (id, name, classroom_email_address, moodle_email_address, url_photo) VALUES (1, 'Marco Taborda', 'marcotaborda.jr@gmail.com', 'marcotaborda.jr@gmail.com', 'https://lh3.googleusercontent.com/a-/AOh14GhwLCRi1rCuqRktm-DLetBGZ9tmE2OdZTB7Ejfbng=s96-c');
